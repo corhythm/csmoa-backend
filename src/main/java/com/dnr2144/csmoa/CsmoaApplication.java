5e0f1a340b2d0e173472f75324d327eb032ecb40
SU_KANG <dnr2144@gmail.com>
2021-10-23 15:42:24 +0900

    project init

---
+++ src/main/java/com/dnr2144/csmoa/CsmoaApplication.java
@@ -0,0 +1,13 @@
+package com.dnr2144.csmoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsmoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsmoaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dnr2144/csmoa/CsmoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnr2144.csmoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsmoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
